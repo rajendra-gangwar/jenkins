d9964a249a199eb955de28074cec63b4706859ad
rajendra-gangwar <73456075+rajendra-gangwar@users.noreply.github.com>
2021-06-29 23:51:15 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 import sys
-print("This is instance_id", sys.argv[0])
-print("This is instance_type", sys.argv[1])
+print("This is instance_id", sys.argv[1])
+print("This is instance_type", sys.argv[2])
