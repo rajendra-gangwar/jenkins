fb2e8061969f3fa9c93a21c4d24223abcdd3e658
rajendra-gangwar <73456075+rajendra-gangwar@users.noreply.github.com>
2021-06-30 01:11:25 +0530

    Update aws.py

--- aws.py
+++ aws.py
@@ -3,21 +3,18 @@ import boto3
 import time
 import sys, os
 
-aws_access_key_id=os.getenv('aws_access_key_id')
-aws_secret_access_key=os.getenv('aws_secret_access_key')
 
-print(aws_access_key_id)
-
-res=boto3.resource('ec2', region_name = 'us-east-1')
-clnt=boto3.client('ec2', region_name = 'us-east-1')
+res=boto3.resource('ec2')
+clnt=boto3.client('ec2')
 inst=res.Instance(sys.argv[1])
-
 stat=inst.state['Name']
 
 
 if stat=='running':
         inst.stop()
         clnt.get_waiter('instance_stopped')
+        inst=res.Instance(sys.argv[1])
+        stat=inst.state['Name']
         print(f"instance id {inst.id} is now in {inst.state['Name']} state" )
 else:
         print("Already in stopped State")
@@ -26,7 +23,8 @@ time.sleep(30)
 response = clnt.modify_instance_attribute(InstanceId = sys.argv[1],InstanceType={'Value': sys.argv[2]})
 clnt.get_waiter('system_status_ok')
 
-
+inst=res.Instance(sys.argv[1])
+stat=inst.state['Name']
 if inst.state['Name']=='stopped':
         inst.start()
         clnt.get_waiter('instance_running')
