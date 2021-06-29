5984a717c459f98aa936e08c82ef796fbd896ea3
rajendra-gangwar <73456075+rajendra-gangwar@users.noreply.github.com>
2021-06-30 01:37:05 +0530

    Update aws.py

--- aws.py
+++ aws.py
@@ -9,25 +9,31 @@ clnt=boto3.client('ec2')
 inst=res.Instance(sys.argv[1])
 stat=inst.state['Name']
 
-
+print("Checking if EC2 Instance is Running")
 if stat=='running':
+        print("Stopping Running Instance")
         inst.stop()
         clnt.get_waiter('instance_stopped')
-        inst=res.Instance(sys.argv[1])
         stat=inst.state['Name']
         print(f"instance id {inst.id} is now in {inst.state['Name']} state" )
 else:
         print("Already in stopped State")
 
-time.sleep(30)
+print("Changing the instance type to ",sys.argv[2])
+time.sleep(60)
 response = clnt.modify_instance_attribute(InstanceId = sys.argv[1],InstanceType={'Value': sys.argv[2]})
-clnt.get_waiter('system_status_ok')
+#clnt.get_waiter('system_status_ok')
+time.sleep(10)
+printf("Starting the Instance")
 
-inst=res.Instance(sys.argv[1])
-stat=inst.state['Name']
-if inst.state['Name']=='stopped':
-        inst.start()
-        clnt.get_waiter('instance_running')
+
+response = client.start_instances(InstanceIds=[sys.argv[2]], AdditionalInfo='string', DryRun=False)
+
+print(response)
+
+'''inst.start()
+clnt.get_waiter('instance_running')
         print(f"instance id {inst.id} is now in {inst.state['Name']} state" )
 else:
         print("Something went wrong")
+'''
