79facf20ab365e15809a34a691d16b0283794b14
rajendra-gangwar <73456075+rajendra-gangwar@users.noreply.github.com>
2021-07-01 12:58:57 +0530

    Update aws_reset.py

--- aws_reset.py
+++ aws_reset.py
@@ -6,22 +6,29 @@ import sys, os
 
 res=boto3.resource('ec2')
 clnt=boto3.client('ec2')
-inst=res.Instance(sys.argv[1])
+
+
+file=open('currtype.txt','w')
+inst_type=file.readline()
+file.close()
+
+file=open('currentid.txt','r')
+inst_id=file.readline()
+file.close()
+
+inst=res.Instance(inst_id)
 stat=inst.state['Name']
 
 print("Checking current InstanceType")
-response = clnt.describe_instance_attribute(Attribute='instanceType', DryRun=False, InstanceId=sys.argv[1])
+response = clnt.describe_instance_attribute(Attribute='instanceType', DryRun=False, InstanceId=inst_id)
 print(response)
 
-file=open('currtype.txt','w')
-inst_type=file.read(currentinst)
-file.close()
 
     
 print("Checking if EC2 Instance is Running")
 if stat=='running':
     print("Stopping Running Instance")
-    response = clnt.stop_instances(InstanceIds=[sys.argv[1],], DryRun=False)
+    response = clnt.stop_instances(InstanceIds=[inst_id,], DryRun=False)
     clnt.get_waiter('instance_stopped')
     print(response)
     print(f"instance id {inst.id} is now in {inst.state['Name']} state" )
@@ -32,12 +39,12 @@ else:
 
 print("Changing the instance type to ",inst_type)
 
-response = clnt.modify_instance_attribute(InstanceId = sys.argv[1],InstanceType={'Value': inst_type})
+response = clnt.modify_instance_attribute(InstanceId = inst_id,InstanceType={'Value': inst_type})
 print("Sleep 10 sec")
 time.sleep(10)
 print(response)
 
 print("Starting the Instance")       
        
-response = clnt.start_instances(InstanceIds= [sys.argv[1],], AdditionalInfo='string', DryRun=False)
+response = clnt.start_instances(InstanceIds= [inst_id,], AdditionalInfo='string', DryRun=False)
 print(response)
