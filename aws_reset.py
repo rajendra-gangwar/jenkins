7adc0e2e8e09706040fffa24f3426dca16e0aa1d
rajendra-gangwar <73456075+rajendra-gangwar@users.noreply.github.com>
2021-07-01 12:24:29 +0530

    Create aws_reset.py

---
+++ aws_reset.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+import boto3
+import time
+import sys, os
+
+
+res=boto3.resource('ec2')
+clnt=boto3.client('ec2')
+inst=res.Instance(sys.argv[1])
+stat=inst.state['Name']
+
+print("Checking current InstanceType")
+response = clnt.describe_instance_attribute(Attribute='instanceType', DryRun=False, InstanceId=sys.argv[1])
+currentinst=response['InstanceType']['Value']
+
+'''if currentinst==sys.argv[2]:
+    print("No change in instance type")
+    break
+else:'''
+os.environ["beforechange"] = currentinst
+print("Current instance type is", currentinst)
+
+print("Checking if EC2 Instance is Running")
+if stat=='running':
+    print("Stopping Running Instance")
+    response = clnt.stop_instances(InstanceIds=sys.argv[1], DryRun=False)
+    clnt.get_waiter('instance_stopped')
+    print(response)
+    print(f"instance id {inst.id} is now in {inst.state['Name']} state" )
+    print("Sleeping for 60 sec")
+    time.sleep(60)
+else:
+        print("Already in stopped State")
+
+print("Changing the instance type to ",sys.argv[2])
+
+response = clnt.modify_instance_attribute(InstanceIds =sys.argv[1],InstanceType={'Value': sys.argv[2]})
+print("Sleep 10 sec")
+time.sleep(10)
+print(response)
+
+print("Starting the Instance")       
+       
+response = clnt.start_instances(InstanceIds= sys.argv[1], AdditionalInfo='string', DryRun=False)
+print(response)
